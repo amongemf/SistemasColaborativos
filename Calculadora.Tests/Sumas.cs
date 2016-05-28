583789d383080c3c03a5aae65a6765b8ffb8c02a
amongemf <amongemf@hotmail.com>
2016-05-28 09:58:28 -0600

    Se incluyen pruenas funcionales

---
+++ Calculadora.Tests/Sumas.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Logica.Negocio;
+
+namespace Calculadora.Tests
+{
+    [TestClass]
+    public class Sumas
+    {
+        [TestMethod]
+        public void SumarUnoMasDos()
+        {
+            //Definir el escenario de la pruba =>Paso 1
+            var elPrimerOperando = 1;
+            var elSegundoOperando = 2;
+            var elResultadoEsperado = 3;
+
+            //Invocar el metodo que se desea validar =>Paso 2
+            var laSuma = new Suma();
+            var elResultadoReal = laSuma.Operar(elPrimerOperando, elSegundoOperando);
+
+            //Verificacion o Comparacion del Resultado obtenido con el esperado =>Paso 3
+            Assert.AreEqual(elResultadoEsperado, elResultadoReal);
+        }
+    }
+}
